37d7acd689b17f4d5c101245afa0e06bd302a966
Billy Olsen <billy.olsen@gmail.com>
2022-04-21 19:42:27 -0700

    Handle mdev devices in libvirt 7.7+
    Libvirt 7.7 changed the mdev device naming to include the parent PCI
    device when listing node devices. The domain, however, will still only
    see the UUID and not see the parent PCI device. Changing the parsing to
    simply drop the PCI identifier is not enough as the device cannot be
    found when attempting to lookup the new ID.
    
    Modify the Libvirt Driver's _get_mediated_device_information to tolerate
    different formats of the mdev name. This first uses the legacy behavior
    by trying to lookup the device name that is passed in (typically
    mdev_<uuid> format) and if that is not found, iterates the list of mdev
    node devices until the right UUID is found and selects that one.
    
    Note that the lookup of the mdev device by UUID are needed in order
    to keep the ability to recreate assigned mediated devices on a reboot of
    the compute node.
    
    Additionally, the libvirt utils parsing method mdev_name2uuid, has
    been updated to tolerate both mdev_<uuid> and mdev_<uuid>_<pciid>
    formats.
    
    Closes-Bug: 1951656
    
    Change-Id: Ifed0fa16053228990a6a8df8d4c666521db7e329
    (cherry picked from commit a28b907c4f0dbba6e141a8fbea807e6cb0438977)
    (cherry picked from commit 98d8c9eaa3c415cc234193e6a9115db887751363)
    (cherry picked from commit 28053917200e3e242148672efda0e1a2b043dc48)

--- nova/virt/libvirt/config.py
+++ nova/virt/libvirt/config.py
@@ -3276,6 +3276,7 @@ class LibvirtConfigNodeDeviceMdevInformation(LibvirtConfigObject):
                                         root_name="capability", **kwargs)
         self.type = None
         self.iommu_group = None
+        self.uuid = None
 
     def parse_dom(self, xmldoc):
         super(LibvirtConfigNodeDeviceMdevInformation,
@@ -3285,6 +3286,8 @@ class LibvirtConfigNodeDeviceMdevInformation(LibvirtConfigObject):
                 self.type = c.get('id')
             if c.tag == "iommuGroup":
                 self.iommu_group = int(c.get('number'))
+            if c.tag == "uuid":
+                self.uuid = c.text
 
 
 class LibvirtConfigGuestRng(LibvirtConfigGuestDevice):
