829f30fbeaabd16b95e6e65cd5a097c781604d1b
Balazs Gibizer <gibi@redhat.com>
2022-06-03 19:33:49 +0200

    Poison /sys access via various calls in test
    Unit test should never depend on the file system of the machine it runs
    on. So this patch injects a poison to various std lib calls to catch
    these cases. We only selectively poison access to /sys fs as there are
    legitimate cases to access temp files.
    
    When I isolated the poisoned test cases I tried to add a meaningful mocking
    around the sysfs reads based on the meaning of the test.
    
    Change-Id: I66636ebfe627cba8c4470cd84faa78f69249aa05

--- nova/test.py
+++ nova/test.py
@@ -286,6 +286,7 @@ class TestCase(base.BaseTestCase):
         quota.UID_QFD_POPULATED_CACHE_ALL = False
 
         self.useFixture(nova_fixtures.GenericPoisonFixture())
+        self.useFixture(nova_fixtures.SysFsPoisonFixture())
 
         # make sure that the wsgi app is fully initialized for all testcase
         # instead of only once initialized for test worker
