5f266201e91837150d6590bdc716097a1036f430
Balazs Gibizer <gibi@redhat.com>
2022-08-08 19:44:41 +0200

    Remove double mocking... again
    I thought we fixed all the double mocking issues with
    I3998d0d49583806ac1c3ae64f1b1fe343cefd20d but I was wrong.
    
    While we used both mock and unittest.mock the fixtures.MockPatch
    used the mock lib instead of the unittest.mock lib.
    The path Ibf4f36136f2c65adad64f75d665c00cf2de4b400 (Remove the PowerVM driver)
    removed the last user of mock lib from nova. So it is also
    removed the mock from test-requirements. This triggered that
    fixtures.MockPatch athat started using unittest.mock too.
    
    Before Ibf4f36136f2c65adad64f75d665c00cf2de4b400 a function can be mocked
    twice once with unittest.mock and once with fixtures.MockPatch (still
    using mock). However after that patch both path of such double
    mocking goes through unittest.mock and the second one fails.
    
    So this patch fixes double mocking so far hidden behind
    fixtures.MockPatch.
    
    This patch made the py310 and functional-py310 jobs voting  on master
    however that has been dropped as part of the backport.
    
    Conflicts:
        nova/tests/fixtures/nova.py
        nova/tests/unit/api/openstack/compute/test_quotas.py
        nova/tests/unit/api/openstack/compute/test_server_group_quotas.py
    
    Conflicts are due to lack of unified limits feature in xena
    
    Change-Id: Ic1352ec31996577a5d0ad18a057339df3e49de25
    (cherry picked from commit bf654e3a4a8f690ad0bec0955690bf4fadf98dba)
    (cherry picked from commit 69667a817cb65c3efbe4e3ada0e8c69c0a106087)

--- nova/tests/fixtures/nova.py
+++ nova/tests/fixtures/nova.py
@@ -923,9 +923,18 @@ class OSAPIFixture(fixtures.Fixture):
             base_url += '/' + self.project_id
 
         self.api = client.TestOpenStackClient(
-            'fake', base_url, project_id=self.project_id)
+            'fake', base_url, project_id=self.project_id,
+        )
+        self.alternative_api = client.TestOpenStackClient(
+            'fake', base_url, project_id=self.project_id,
+        )
         self.admin_api = client.TestOpenStackClient(
-            'admin', base_url, project_id=self.project_id)
+            'admin', base_url, project_id=self.project_id,
+        )
+        self.alternative_admin_api = client.TestOpenStackClient(
+            'admin', base_url, project_id=self.project_id,
+        )
+
         # Provide a way to access the wsgi application to tests using
         # the fixture.
         self.app = app
@@ -1013,9 +1022,9 @@ class PoisonFunctions(fixtures.Fixture):
         # Don't poison the function if it's already mocked
         import nova.virt.libvirt.host
         if not isinstance(nova.virt.libvirt.host.Host._init_events, mock.Mock):
-            self.useFixture(fixtures.MockPatch(
+            self.useFixture(fixtures.MonkeyPatch(
                 'nova.virt.libvirt.host.Host._init_events',
-                side_effect=evloop))
+                evloop))
 
 
 class IndirectionAPIFixture(fixtures.Fixture):
