9c5fed0faea2951a621247d2e04db775c22ce167
Dan Smith <dansmith@redhat.com>
2023-01-26 11:46:43 -0800

    Fixup patch for stable-compute-uuid series
    Minor cleanups from feedback on earlier patches.
    
    Related to blueprint stable-compute-uuid
    
    Change-Id: I00505f1df47b46ed36645c781354258e255f0dcc

--- nova/tests/functional/integrated_helpers.py
+++ nova/tests/functional/integrated_helpers.py
@@ -1228,8 +1228,6 @@ class _IntegratedTestBase(test.TestCase, PlacementInstanceHelperMixin):
         self.glance = self.useFixture(nova_fixtures.GlanceFixture(self))
         self.policy = self.useFixture(nova_fixtures.RealPolicyFixture())
 
-        self.useFixture(nova_fixtures.ComputeNodeIdFixture())
-
         self.notifier = self.useFixture(
             nova_fixtures.NotificationFixture(self))
 
@@ -1300,8 +1298,6 @@ class ProviderUsageBaseTestCase(test.TestCase, PlacementInstanceHelperMixin):
         self.glance = self.useFixture(nova_fixtures.GlanceFixture(self))
         self.placement = self.useFixture(func_fixtures.PlacementFixture()).api
         self.useFixture(nova_fixtures.AllServicesCurrent())
-
-        self.useFixture(nova_fixtures.ComputeNodeIdFixture())
 
         self.notifier = self.useFixture(
             nova_fixtures.NotificationFixture(self))
