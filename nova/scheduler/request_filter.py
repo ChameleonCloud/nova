0e86c0b4efe4daf9150dc769147e1e22a77d41a2
Mike <shermanm@uchicago.edu>
2024-05-01 14:18:43 -0500

    add nova prefilter for blazar (#11)
    As a workaround for nova issue https://bugs.launchpad.net/nova/+bug/1542491
    
    This PR implements the blazar-nova filter as a nova prefilter, taking action before a request is sent to placement for scheduling candidates.
    
    Two config items are added to nova's "[scheduler]" section
    ```
    blazar_reservation_required = True # whether the prefilter should deny all requests with no reservation hint
    use_blazar_reservation_prefilter = True # whether to enable this prefilter
    ```

--- nova/scheduler/request_filter.py
+++ nova/scheduler/request_filter.py
@@ -30,7 +30,6 @@ CONF = nova.conf.CONF
 LOG = logging.getLogger(__name__)
 TENANT_METADATA_KEY = 'filter_tenant_id'
 
-
 def trace_request_filter(fn):
     @functools.wraps(fn)
     def wrapper(ctxt, request_spec):
@@ -132,6 +131,83 @@ def require_tenant_aggregate(ctxt, request_spec):
         raise exception.RequestFilterFailed(
             reason=_('No hosts available for tenant'))
 
+    return True
+
+@trace_request_filter
+def blazar_reservation_filter(ctxt, request_spec):
+    """Require hosts to be in an aggregate corresponding to a blazar reservation.
+    The reservation_id must be passed in as a scheduler hint, with key `reservation`
+
+    Additionally, the user must have permission to use the corresponding reservation.
+    For now, this is enforced by checking that the value 'blazar:owner' on the aggregate
+    matches the requesting project_id.
+
+    if reservation_required is set to true, any scheduling requests without a reservation
+    hint will be rejected.
+
+    In terms of behavior, this is just a combination of the map_az_to_placement_aggregate
+    and require_tenant_aggregate filters, with different key names.
+    """
+
+    # skip the filter if not enabled
+    enabled = CONF.scheduler.use_blazar_reservation_prefilter
+    if not enabled:
+        return False
+
+    # allow setting reservation hint as mandatory
+    reservation_required = CONF.scheduler.blazar_reservation_required
+    reservation_hints = request_spec.scheduler_hints.get('reservation', None)
+
+    # handle missing reservation hint
+    if not reservation_hints:
+        no_reservation_hint_msg = f"Can't schedule instance {request_spec.instance_uuid}. No reservation hint was specified."
+        if reservation_required:
+            LOG.warning(no_reservation_hint_msg)
+            raise exception.RequestFilterFailed(reason=_(no_reservation_hint_msg))
+        else:
+            LOG.info(no_reservation_hint_msg)
+            # if no reservation hint, we'll skip the rest of this prefilter
+            return False
+
+    # after this point, assume we have a reservation hint. we don't check for
+    # "reservation required" any longer. At this point, you requested a reservation,
+    # and none matched, so 0 is the expected result. If you didn't want one,
+    # you shouldn't have asked for one!
+
+
+    # handle possibility of multiple reservation hints, or malformed requests by
+    # not asking DB directly for the name, instead get list that project "could" use.
+    aggregates_for_project_id = objects.AggregateList.get_by_metadata(ctxt,
+                                                                      key='blazar:owner',
+                                                                      value=request_spec.project_id)
+
+    if not aggregates_for_project_id:
+        no_project_aggregates_msg=f"Can't schedule instance {request_spec.instance_uuid}. Tenant {request_spec.project_id} has no available aggregates"
+        LOG.warning(no_project_aggregates_msg)
+        raise exception.RequestFilterFailed(reason=_(no_project_aggregates_msg))
+
+    # of the aggregates owned by the project, check for ones with the right name
+    aggregate_uuids_for_reservation = set([])
+    for agg in aggregates_for_project_id:
+        if agg.name in reservation_hints:
+            aggregate_uuids_for_reservation.add(agg.uuid)
+            break
+
+    # handle common case, we've found an aggregate to use, ask placement to filter by it
+    if aggregate_uuids_for_reservation:
+        if ('requested_destination' not in request_spec or request_spec.requested_destination is None):
+            request_spec.requested_destination = objects.Destination()
+        destination = request_spec.requested_destination
+        destination.require_aggregates(aggregate_uuids_for_reservation)
+        LOG.debug('blazar_reservation_filter request filter added aggregates %s for tenant %r',
+                  ','.join(aggregate_uuids_for_reservation), request_spec.project_id)
+    else:
+        # no aggregates match
+        no_aggregates_for_reservation_hint_msg=f"Can't schedule instance {request_spec.instance_uuid}. No aggregates were found matching hints {reservation_hints}"
+        LOG.warning(no_aggregates_for_reservation_hint_msg)
+        raise exception.RequestFilterFailed(reason=_(no_aggregates_for_reservation_hint_msg))
+
+    # at this point, we've either raised an exception or successfully updated the destination aggregate
     return True
 
 
@@ -435,6 +511,7 @@ ALL_REQUEST_FILTERS = [
     map_az_to_placement_aggregate,
     require_image_type_support,
     compute_status_filter,
+    blazar_reservation_filter,
     isolate_aggregates,
     transform_image_metadata,
     accelerators_filter,
